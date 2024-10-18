e429ac7a3eb0878f4d8140d835377b8198ba6d88
Ilya Svintsov <xeri237@gmail.com>
2024-10-18 13:15:49 +0300

    new commit

--- src/server.js
+++ src/server.js
@@ -19,12 +19,12 @@ export const setupServer = () => {
     );
 
     app.get('/contacts', async (req, res) => {
-        const students = await getAllContacts();
+        const contacts = await getAllContacts();
 
         res.status(200).json({
             status: 200,
             message: "Successfully found contacts!",
-            data: students
+            data: contacts
         })
     })
 
